a92ae34d8861a4dbfedff3709ac75dbd63cf2690
lutfioz <luttfioz@gmail.com>
2019-10-24 10:37:19 +0300

    Add home page

--- lab05/shopping-services-app/app.js
+++ lab05/shopping-services-app/app.js
@@ -5,6 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
 var routesApi = require('./app_api/routes/index');
 
 var app = express();
@@ -19,6 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
 app.use('/api', routesApi);
 
 // catch 404 and forward to error handler
