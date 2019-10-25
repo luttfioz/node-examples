a6b6088956325f98ce7adeb3a66e2ca1e71d97a8
lutfioz <luttfioz@gmail.com>
2019-10-25 10:41:37 +0300

    Add cors

--- lab05/shopping-services-app/app.js
+++ lab05/shopping-services-app/app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var routesApi = require('./app_api/routes/index');
@@ -13,7 +14,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
