7a6bc6c670316f62e9df0b8a3e65e64fe2cf81e8
lutfioz <luttfioz@gmail.com>
2019-10-25 14:34:14 +0300

    Add react-native

---
+++ 01-presentations/react-native/gitlab-react-native.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+mkdir ReactNative
+cd ReactNative
+echo -e "CLONING..."
+echo "Cloning react-native repos from gitlab.eteration.com"
+git clone https://gitlab.eteration.com/academy/tutorials/react-native/react-native-intl.git
+git clone https://gitlab.eteration.com/academy/tutorials/react-native/react-native-saga.git
+git clone https://gitlab.eteration.com/academy/tutorials/react-native/react-native-redux.git
+git clone https://gitlab.eteration.com/academy/tutorials/react-native/react-native-routing.git
+git clone https://gitlab.eteration.com/academy/tutorials/react-native/react-native-components.git
+git clone https://gitlab.eteration.com/academy/tutorials/react-native/react-native-styling.git
+git clone https://gitlab.eteration.com/academy/tutorials/react-native/react-native-quickstart.git
+echo "CLONING PROCESS DONE..............."
+
+echo "------------CLONE -> DONE ! ------ "
+
